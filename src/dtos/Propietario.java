29558314794adf8c79ea1a98e899110ee63206e3
DESKTOP-A7UOEK4\Jaime <csi22-jromrez@colegioaltair.net>
2024-06-07 12:45:50 +0200

    no message

--- src/dtos/Propietario.java
+++ src/dtos/Propietario.java
@@ -9,4 +9,31 @@ public class Propietario {
 	String FechaCompra;
 	String Matricula;
 	
+	//Getter y Setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDni() {
+		return Dni;
+	}
+	public void setDni(String dni) {
+		Dni = dni;
+	}
+	public String getFechaCompra() {
+		return FechaCompra;
+	}
+	public void setFechaCompra(String fechaCompra) {
+		FechaCompra = fechaCompra;
+	}
+	public String getMatricula() {
+		return Matricula;
+	}
+	public void setMatricula(String matricula) {
+		Matricula = matricula;
+	}
+	
+	
 }
--- src/dtos/Vehiculo.java
+++ src/dtos/Vehiculo.java
@@ -8,4 +8,26 @@ public class Vehiculo {
 	String Matricula;
 	
 	
+	//Getter y Setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFechaMatriculacion() {
+		return FechaMatriculacion;
+	}
+	public void setFechaMatriculacion(String fechaMatriculacion) {
+		FechaMatriculacion = fechaMatriculacion;
+	}
+	public String getMatricula() {
+		return Matricula;
+	}
+	public void setMatricula(String matricula) {
+		Matricula = matricula;
+	}
+	
+	
+	
 }
